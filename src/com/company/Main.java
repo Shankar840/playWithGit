844ae52ee9a5e05257a9c9ece07e24c935fb3899
Shankar840 <82820759+Shankar840@users.noreply.github.com>
2021-06-18 18:39:33 +0530

    Update Main.java and added file1.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is First line");
         System.out.println("This is SecondQ line");
+        System.out.println("This is to demonstate the fetch + merge");
     }
 }
