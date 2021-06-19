75bcce4b2a3ed0699738bc6b0259d817e64aa599
Shankar840 <s.p.shankar.na@gmail.com>
2021-06-19 15:59:00 +0530

    Revert "first commit + second commit for revert jump"
    This reverts commit 16f603eb9666f69c6d2dd20e307cb9f38aa20337.

--- src/com/company/GitPull.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-class GitPull{
-    public static void main(String[] args) {
-        System.out.println("this is vfirst commit for revert jump");
-        System.out.println("this is second for revert jump");
-    }
-}
