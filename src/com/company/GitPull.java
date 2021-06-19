ec85a2e6ed130ab53217b9be1d18e6adbd73da44
Shankar840 <s.p.shankar.na@gmail.com>
2021-06-19 15:44:56 +0530

    second commit for revert jump

--- src/com/company/GitPull.java
+++ src/com/company/GitPull.java
@@ -2,5 +2,6 @@ package com.company;
 class GitPull{
     public static void main(String[] args) {
         System.out.println("this is vfirst commit for revert jump");
+        System.out.println("this is second for revert jump");
     }
 }
